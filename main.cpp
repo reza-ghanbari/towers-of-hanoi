1ccedf03ec01f73d02965934dfa0e2314585c0d4
reza-ghanbari <rezaqanbari1925@gmail.com>
2023-03-20 19:05:45 -0600

    add solver

--- main.cpp
+++ main.cpp
@@ -10,26 +10,28 @@ State* createRoot() {
     auto* topDiskInPegs = new Short[NUMBER_OF_PEGS]{0};
     for (int i = 1; i < NUMBER_OF_PEGS; ++i)
         topDiskInPegs[i] = TOWER_SIZE;
-    return new State(state
+    State* root = new State(state
             , numberOfDisksInPegs
             , topDiskInPegs
             , TOWER_SIZE);
+    root->setGCost(0);
+    return root;
 }
 
 int main() {
+    std::cout << "pdb creating started..." << std::endl;
     auto start = std::chrono::steady_clock::now();
-    auto* heuristic = new Heuristic<Long>(ABSTRACT_SIZE);
-    auto* smallHeuristic = new Heuristic<SmallInt>(TOWER_SIZE - ABSTRACT_SIZE);
-    std::cout << "solving started..." << std::endl;
-    auto* solver = new Solver(createRoot(), heuristic, smallHeuristic);
-    solver->solve();
+    auto* heuristic = new Heuristic<Long>(ABSTRACT_SIZE, "pdb-long.txt");
+    auto* smallHeuristic = new Heuristic<SmallInt>(TOWER_SIZE - ABSTRACT_SIZE, "pdb-short.txt");
     auto end = std::chrono::steady_clock::now();
     std::cout << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "s" << std::endl;
-//    std::cout << "saving to file" << std::endl;
-//    start = std::chrono::steady_clock::now();
-//    heuristic->saveToFile("pdb-long.txt");
-//    end = std::chrono::steady_clock::now();
-//    std::cout << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "s" << std::endl;
-//    smallHeuristic->saveToFile("pdb-short.txt");
+    std::cout << "solving started..." << std::endl;
+    auto* solver = new Solver(createRoot(), heuristic, smallHeuristic);
+    start = std::chrono::steady_clock::now();
+    solver->solve();
+    end = std::chrono::steady_clock::now();
+    std::cout << "time: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "s" << std::endl;
+//    heuristic->saveToFile("pdb-long1.txt");
+//    smallHeuristic->saveToFile("pdb-short1.txt");
     return 0;
 }
